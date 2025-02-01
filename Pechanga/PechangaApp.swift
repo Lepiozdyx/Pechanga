ef4bd2ceccbbbb2929f0427688df4cc4ff9a0870
Developer <Lepiozdyx@gmail.com>
2025-02-01 14:21:10 -0300

    Initial Commit

---
+++ Pechanga/PechangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PechangaApp.swift
+//  Pechanga
+//
+//  Created by Alex on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PechangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
